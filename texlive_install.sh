a14689cd7c25943e828b1f43b40864ff5dfa9e90
Ross Dempsey <rossd97@gmail.com>
2020-08-11 16:27:23 -0400

    Update texlive_install.sh

--- texlive_install.sh
+++ texlive_install.sh
@@ -31,7 +31,8 @@ tlmgr install   \
   tufte-latex   \
   forest        \
   xkeyval       \
-  hardwrap
+  hardwrap      \
+  catchfile
 
 # Keep no backups (not required, simply makes cache bigger)
 tlmgr option -- autobackup 0
