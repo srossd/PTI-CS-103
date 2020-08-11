2260ee2d186431fa4dcee9b002fbf7cb95f0eb61
Ross Dempsey <rossd97@gmail.com>
2020-08-11 16:20:54 -0400

    Update texlive_install.sh

--- texlive_install.sh
+++ texlive_install.sh
@@ -28,7 +28,8 @@ tlmgr install luatex
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
 # texlive-latex-base is needed to run pdflatex
 tlmgr install   \
-  tufte-book
+  tufte-latex   \
+  forest
 
 # Keep no backups (not required, simply makes cache bigger)
 tlmgr option -- autobackup 0
