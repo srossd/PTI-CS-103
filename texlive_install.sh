d9e38cceee60a93aa4ea8090d5663205083e4868
Ross Dempsey <rossd97@gmail.com>
2020-08-11 16:44:19 -0400

    Update texlive_install.sh

--- texlive_install.sh
+++ texlive_install.sh
@@ -21,13 +21,10 @@ if ! command -v texlua > /dev/null; then
   cd ..
 fi
 
-# Just including texlua so the cache check above works
-# Needed for any use of texlua even if not testing LuaTeX
-tlmgr install luatex
-
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
 # texlive-latex-base is needed to run pdflatex
 tlmgr install   \
+  luatex        \
   tufte-latex   \
   forest        \
   xkeyval       \
@@ -36,7 +33,9 @@ tlmgr install   \
   titlesec      \
   ragged2e      \
   ms            \
-  textcase
+  textcase      \
+  setspace      \
+  soul
 
 # Keep no backups (not required, simply makes cache bigger)
 tlmgr option -- autobackup 0
